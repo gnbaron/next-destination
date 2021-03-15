7fa0fb5039e2e6c6eac3dc5bfdd5ace382087e15
Guilherme Baron <floriani.guilherme@gmail.com>
2021-03-15 19:45:50 -0300

    add styled components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next Destination</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta name="description" content="Destination explorer app" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
